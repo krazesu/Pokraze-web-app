1d3b59e9a6418f4d15a748919506eb6b34b0eb7e
Kriz Greg <krizgreg.urmeneta.work@gmail.com>
2026-05-04 23:43:44 +0800

    initial commit

---
+++ Pokraze-web-app/src/App.jsx
@@ -0,0 +1,12 @@
+import TrainerCard from './components/TrainerCard.jsx';
+
+function App() {
+  return(
+    <>
+      <h1>Pokraze</h1>
+      <TrainerCard />
+    </>
+  );
+}
+
+export default App
