c9ae667015aaec4f06c97afe612f11c7a3ecee20
Kriz Greg <krizgreg.urmeneta.work@gmail.com>
2026-05-11 18:17:20 +0800

    integrate backend API to frontend

--- Pokraze-web-app/server/services/pokemon.service.js
+++ Pokraze-web-app/server/services/pokemon.service.js
@@ -1,16 +1,35 @@
 const Pokemon = require('../models/pokemon')
 
 const searchPokemon = async (pokemonName) => {
+    //fetch pokemon data from pokemon API
     const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
     );
 
     if (!response.ok) {
-        throw new Error("Pokemon not found");
+        throw new Error("Pokemon Not Found!");
     }
 
-    const data = await response.json();
-    return data;
+    //get pokemon object
+    const pokemon = await response.json();
+
+    //get the species data to get the description
+    const speciesRes = await fetch(pokemon.species.url);
+    const speciesData = await speciesRes.json();
+
+    const entry = speciesData.flavor_text_entries.find(
+        (e) => e.language.name === "en"
+    );
+
+    const description = entry
+        ? entry.flavor_text.replace(/\f/g, " ")
+        : "";
+
+    // return pokemon object and description
+    return {
+        pokemon,
+        description,
+    };
 }
 
 const updateSearchCount = async (pokemonName) => {
