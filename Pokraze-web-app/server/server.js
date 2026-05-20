6a1962c0b0c8f937945764a2d9132e2ac15db59a
Kriz Greg <krizgreg.urmeneta.work@gmail.com>
2026-05-21 07:53:04 +0800

    fix log in issue

--- Pokraze-web-app/server/server.js
+++ Pokraze-web-app/server/server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.ATLAS_URL_DEVELOPMENT)
+mongoose.connect(process.env.ATLAS_URL)
 const db = mongoose.connection
 
 // listen for DB connection errors
